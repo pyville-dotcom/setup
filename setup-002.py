4194db2d31c7826f927c1bce658c8dde4b2edf80
pyville-dotcom <pyvillecom@gmail.com>
2024-11-16 17:55:35 -0500

    Update setup-002.py

--- setup-002.py
+++ setup-002.py
@@ -10,12 +10,21 @@ if not os.path.isdir('data'):
 
 con = sqlite3.connect('data/db')
 
+dtype = {
+    'sales': 'INT',
+    'store': 'CAHR(3)',
+    'city': 'VARCHAR(50)',
+    'country': 'VARCHAR(50)',
+    'continent': 'VARCHAR(50)',
+}
+
 tsvs = [f for f in  os.listdir() if f.split('.')[-1] == 'tsv']
+
 for tsv in tsvs:
   df = pd.read_csv(tsv, sep='\t')
   x = tsv[:-4]
   df.to_csv(f'data/{x}.csv', index=False)
   df.to_excel(f'data/{x}.xlsx', index=False)
-  df.to_sql(x, index=False, con=con, if_exists='replace')
+  df.to_sql(x, index=False, con=con, if_exists='replace', dtype=dtype)
 
 print("Done.")
